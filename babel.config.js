c48a4a89a6867a9336cc3207c1da52379829342b
Frank Calise <fcalise@gmail.com>
2025-09-23 22:10:09 -0400

    chore: add react-native-worklets
    still receive web error

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,7 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ["babel-preset-expo"],
+    // needed for web as per https://github.com/software-mansion/react-native-reanimated/blob/main/apps/web-example/babel.config.js#L35
+    plugins: ["react-native-worklets/plugin"],
   }
 }
